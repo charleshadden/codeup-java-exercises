e749cea45a508ffa163d43117f632fbdf2d83ec1
charleshadden <charles.r.hadden@gmail.com>
2018-07-25 12:24:24 -0500

    added consoleflow

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,11 @@
 public class HelloWorld {
         public static void main(String[] args) {
             System.out.println("Hello, World!");
+            int myFavoriteNumber = 7;
+            System.out.println("My favorite number is : " + myFavoriteNumber);
+            int x = 5;
+            System.out.println(++x);
+            System.out.println(x);
         }
     }
 
